bfc0be551caec758f30e87d78edc8a1151021d46
Ivson Santos <santosivson@hotmail.com>
2018-03-25 18:49:24 -0300

    first commit

---
+++ src/main/java/com/ivson/spring/SbSecurityThymeleafApplication.java
@@ -0,0 +1,12 @@
+package com.ivson.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbSecurityThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbSecurityThymeleafApplication.class, args);
+	}
+}
